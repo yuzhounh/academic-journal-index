b7c90d1d5d2a907230882e5f70a43e7b7d295bcb
Jing Wang <yuzhounh@163.com>
2026-08-11 23:43:11 +0800

    Switch Firebase project to academic-journal-index

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "journal-information-5664-d4572",
-  "appId": "1:851079863796:web:83e767c3c55ee1d750ada1",
+  "projectId": "academic-journal-index",
+  "appId": "PENDING_UPDATE_FROM_FIREBASE_CONSOLE",
   "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "journal-information-5664-d4572.firebaseapp.com",
+  "authDomain": "academic-journal-index.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "851079863796"
+  "messagingSenderId": "PENDING_UPDATE_FROM_FIREBASE_CONSOLE"
 };
